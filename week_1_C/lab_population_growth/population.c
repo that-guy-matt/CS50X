aa3c2308b68c1693be29369286179067450d8286
that-guy-matt <matteidsen@gmail.com>
2023-08-20 12:37:20 -0700

    added week 1 lab population.c

---
+++ week_1_C/lab_population_growth/population.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start_size;
+    do
+    {
+        start_size = get_int("Start size: ");
+    }
+    while (start_size < 9);
+
+    // Prompt for end size
+    int end_size;
+    do
+    {
+        end_size = get_int("End size: ");
+    }
+    while (end_size < start_size);
+
+    // Calculate number of years until we reach threshold
+    int years = 0;
+    while (start_size < end_size)
+    {
+        start_size = start_size + (start_size / 3) - (start_size / 4);
+        years++;
+        // this was just used to check where the calculation was at for debugging purposes
+        // printf("Population this year: %i\n", start_size);
+    }
+    // Print number of years
+    printf("Years: %i\n", years);
+}
