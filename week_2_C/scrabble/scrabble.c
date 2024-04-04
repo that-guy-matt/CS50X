043afe30ce801c4a4742ca99cc79741f03e5f3b4
Matthew <matteidsen@gmail.com>
2024-04-03 22:46:32 -0700

    start scrabble solution, create starting code

---
+++ week_2_C/scrabble/scrabble.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+string getPlayerWord();
+int computePlayerScore(int n);
+void computeWinner(int p1, int p2);
+
+int main(void)
+{
+    char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    int points[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+    int p1Score = 0;
+    int p2Score = 0;
+
+}
+
+string getPlayerWord(){
+    // TODO get player word
+}
+
+int computePlayerScore(int n){
+    // TODO computer player score
+}
+
+void computeWinner(int p1, int p2){
+    // TODO compute winner
+}
